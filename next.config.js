a8f20ef23602cb5ccb01c2ff51ca831090aaa5ef
junoryu <juno@acon3d.com>
2024-03-19 16:51:42 +0900

    initial setting, setup vecel domain, setup nextUI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/lol/:path*",
+  //       destination: "https://kr.api.riotgames.com/lol/:path*",
+  //     },
+  //     {
+  //       source: "/asia/lol/:path*",
+  //       destination: "https://asia.api.riotgames.com/lol/:path*",
+  //     },
+  //     {
+  //       source: "/cdn/12.20.1/:path*",
+  //       destination: "http://ddragon.leagueoflegends.com/:path*",
+  //     },
+  //   ];
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
